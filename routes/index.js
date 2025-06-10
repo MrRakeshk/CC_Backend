3167ceb19ff0bdf3bac0467b5562e0bd89f0044f
Rakesh <112494957+MrRakeshk@users.noreply.github.com>
2025-06-10 18:36:26 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,21 +3,21 @@ const userRouter = require("./user");
 const jobRouter = require("./job");
 const applicationRouter = require("./applications");
 const ratingRouter = require("./rating");
-const uploadRouter = require("./uploadImage");
-const ResumeRouter = require("./uploadResume");
-const downloadRouter = require("./download");
+const uploadImageRouter = require("./uploadImage");
+const uploadResumeRouter = require("./uploadResume");
+const downloadResumeRouter = require("./downloadResume");
 const applicantRouter = require("./applicant");
 
 const initRouter = (app) => {
-  app.use("/api/auth", authRouter);
-  app.use("/api/user", userRouter);
-  app.use("/api/jobs", jobRouter);
-  app.use("/api/applications", applicationRouter);
-  app.use("/api/rating", ratingRouter);
-  app.use("/api/upload", uploadRouter);
-  app.use("/api/uploadResume", ResumeRouter);
-  app.use("/api/download", downloadRouter);
-  app.use("/api/applicants", applicantRouter);
+  app.use("/api/auth", authRouter);
+  app.use("/api/user", userRouter);
+  app.use("/api/jobs", jobRouter);
+  app.use("/api/applications", applicationRouter);
+  app.use("/api/rating", ratingRouter);
+  app.use("/api/upload/image", uploadImageRouter);
+  app.use("/api/upload/resume", uploadResumeRouter);
+  app.use("/api/download/resume", downloadResumeRouter);
+  app.use("/api/applicants", applicantRouter);
 };
 
 module.exports = initRouter;

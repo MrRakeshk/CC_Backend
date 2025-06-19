eead4b92346c14f8844d47667db918ffd28ba249
Rakesh <112494957+MrRakeshk@users.noreply.github.com>
2025-06-19 14:46:46 +0530

    Update sendMail.js

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -1,11 +1,27 @@
-await sendMail({
-  email: user.email,
-  subject: "Reset Your Password",
-  html: `
-    <p>You received this email because you (or someone else) requested a password reset.</p>
-    <p><a href="${resetLink}">Click here to reset your password</a></p>
-    <p>This link will expire in 15 minutes.</p>
-    <br>
-    <p>If you didn't request this, you can safely ignore this email.</p>
-  `,
-});
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+// Function to send email using nodemailer
+const sendMail = async ({ email, html, subject }) => {
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.MY_EMAIL,
+      pass: process.env.MY_PASSWORD,
+    },
+  });
+
+  const info = await transporter.sendMail({
+    from: '"Job Portal Support" <no-reply@jobportal.com>',
+    to: email,
+    subject: subject,
+    html: html,
+  });
+
+  return info;
+};
+
+module.exports = sendMail;
+
